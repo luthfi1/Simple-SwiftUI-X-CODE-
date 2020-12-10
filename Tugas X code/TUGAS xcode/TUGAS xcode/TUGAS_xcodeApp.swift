785c2696a61117d5f0589a3748633d749cc49821
luthfi1 <68719137+luthfi1@users.noreply.github.com>
2020-12-10 16:46:12 +0700

    Add files via upload

---
+++ Tugas X code/TUGAS xcode/TUGAS xcode/TUGAS_xcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TUGAS_xcodeApp.swift
+//  TUGAS xcode
+//
+//  Created by muhammad luthfi farizqi on 10/12/20.
+//
+
+import SwiftUI
+
+@main
+struct TUGAS_xcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
